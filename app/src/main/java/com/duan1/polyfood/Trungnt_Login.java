edc50e25539d6737bf5229d7f7056dd169eb27eb
TrungNTPH <trungntph49619@gmail.com>
2024-11-05 09:36:35 +0700

    Đăng ký và đăng nhập thông qua Email tích hợp Firebase

--- app/src/main/java/com/duan1/polyfood/Trungnt_Login.java
+++ app/src/main/java/com/duan1/polyfood/Trungnt_Login.java
@@ -1,26 +1,45 @@
 package com.duan1.polyfood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Trungnt_Login extends AppCompatActivity {
 
-    private EditText edtPassword, edtUserName;
+    private EditText edtPassword, edtEmail;
     private ImageView ivEye;
     private boolean isPasswordVisible = false;
+    private TextView txtRegisNow;
+    private Button btnLogin;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trungnt_login);
-
+        auth = FirebaseAuth.getInstance();
         edtPassword = findViewById(R.id.edtPassword);
         ivEye = findViewById(R.id.ivEye);
+        txtRegisNow = findViewById(R.id.txtRegisNow);
+        btnLogin = findViewById(R.id.btnLogin);
+        edtEmail = findViewById(R.id.edtEmail);
 
         ivEye.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +57,50 @@ public class Trungnt_Login extends AppCompatActivity {
                 edtPassword.setSelection(edtPassword.length()); // Đặt con trỏ về cuối EditText
             }
         });
+        txtRegisNow.setOnClickListener(view -> {
+            Intent intent = new Intent(Trungnt_Login.this, Trungnt_Register.class);
+            startActivity(intent);
+        });
+        btnLogin.setOnClickListener(view -> {
+            String email = edtEmail.getText().toString().trim();
+            Log.e("aa", "onCreate: "+email );
+            String password = edtPassword.getText().toString().trim();
+            Log.e("aa", "onCreate: "+password );
+            boolean isValid = validate(email, password);
+            if (isValid){
+                auth.signInWithEmailAndPassword(email, password).
+                        addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(Trungnt_Login.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(Trungnt_Login.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    private boolean validate(String email, String password){
+        if (email.isEmpty()) {
+            edtEmail.setError("Email không được để trống");
+            return false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edtEmail.setError("Email không hợp lệ");
+            return false;
+        }
+        if (password.length() < 6) {
+            edtPassword.setError("Mật khẩu phải dài hơn 6 ký tự");
+            return false;
+        }
+        if (password.isEmpty()) {
+            edtPassword.setError("Mật khẩu không được để trống");
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/duan1/polyfood/Trungnt_Register.java
+++ app/src/main/java/com/duan1/polyfood/Trungnt_Register.java
@@ -1,24 +1,152 @@
 package com.duan1.polyfood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Trungnt_Register extends AppCompatActivity {
+
+    private TextView txtLoginNow;
+    private ImageView ivEye1, ivEye2;
+    private boolean isPasswordVisible = false;
+    private EditText edtUserNameRegis, edtEmailRegis, edtPasswordRegis, edtConfirmRegis;
+    private Button btnRegister;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_trungnt_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        auth = FirebaseAuth.getInstance();
+        edtUserNameRegis = findViewById(R.id.edtUserNameRegis);
+        edtEmailRegis = findViewById(R.id.edtEmailRegis);
+        edtPasswordRegis = findViewById(R.id.edtPasswordRegis);
+        edtConfirmRegis = findViewById(R.id.edtConfirmRegis);
+        ivEye1 = findViewById(R.id.ivEye1);
+        ivEye2 = findViewById(R.id.ivEye2);
+        txtLoginNow = findViewById(R.id.txtLoginNow);
+        btnRegister = findViewById(R.id.btnRegister);
+        txtLoginNow.setOnClickListener(view -> {
+            Intent intent = new Intent(Trungnt_Register.this, Trungnt_Login.class);
+            startActivity(intent);
+        });
+        ivEye1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    // Ẩn mật khẩu
+                    edtPasswordRegis.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    ivEye1.setImageResource(R.drawable.hide_icon); // Thay đổi biểu tượng thành mắt đóng
+                } else {
+                    // Hiện mật khẩu
+                    edtPasswordRegis.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    ivEye1.setImageResource(R.drawable.hide_icon); // Thay đổi biểu tượng thành mắt mở
+                }
+                isPasswordVisible = !isPasswordVisible;
+                edtPasswordRegis.setSelection(edtPasswordRegis.length()); // Đặt con trỏ về cuối EditText
+            }
+        });
+        ivEye2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    // Ẩn mật khẩu
+                    edtConfirmRegis.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    ivEye2.setImageResource(R.drawable.hide_icon); // Thay đổi biểu tượng thành mắt đóng
+                } else {
+                    // Hiện mật khẩu
+                    edtConfirmRegis.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    ivEye2.setImageResource(R.drawable.hide_icon); // Thay đổi biểu tượng thành mắt mở
+                }
+                isPasswordVisible = !isPasswordVisible;
+                edtConfirmRegis.setSelection(edtConfirmRegis.length()); // Đặt con trỏ về cuối EditText
+            }
+        });
+        btnRegister.setOnClickListener(view -> {
+            String password = edtPasswordRegis.getText().toString().trim();
+            String confirmPassword = edtConfirmRegis.getText().toString().trim();
+            String email = edtEmailRegis.getText().toString().trim();
+            String user = edtUserNameRegis.getText().toString().trim();
+            boolean isPasswordValid = check(password, confirmPassword, email, user);
+
+            if (isPasswordValid){
+
+                Log.e("aa", "onCreate: "+email );
+                Log.e("aa", "onCreate: "+password );
+                auth.createUserWithEmailAndPassword(email,
+                        password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser currentUser = auth.getCurrentUser();
+                            Toast.makeText(Trungnt_Register.this, "Dang ky Thanh cong"+currentUser, Toast.LENGTH_SHORT).show();
+                        }else{
+                            Log.e("FirebaseAuthError", "Đăng ký thất bại", task.getException());
+                            Toast.makeText(Trungnt_Register.this, "Dang ky that bai", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }else {
+//                Toast.makeText(this, "Mật khẩu không hợp lệ", Toast.LENGTH_SHORT).show();
+            }
         });
     }
+
+    private Boolean check(String password, String confirmPassword, String email, String user) {
+
+        if (user.isEmpty()) {
+            edtUserNameRegis.setError("Tên người dùng không được để trống");
+            return false;
+        }
+        if (email.isEmpty()) {
+            edtEmailRegis.setError("Email không được để trống");
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edtEmailRegis.setError("Email không hợp lệ");
+            return false;
+        }
+        if (password.isEmpty()) {
+            edtPasswordRegis.setError("Mật khẩu không được để trống");
+            return false;
+        }
+        // Kiểm tra độ dài mật khẩu
+        if (password.length() < 6) {
+            edtPasswordRegis.setError("Mật khẩu phải dài hơn 6 ký tự");
+            return false;
+        }
+        // Kiểm tra xác nhận mật khẩu
+        if (!password.equals(confirmPassword)) {
+            edtConfirmRegis.setError("Mật khẩu xác nhận không khớp");
+            return false;
+        }
+
+
+
+        return true;
+    }
+
 }
